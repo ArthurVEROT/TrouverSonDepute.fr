5da9767dccec3dcd20c891f82c2061521983310a
Arthur Vrt <46626919+ArthurVEROT@users.noreply.github.com>
2022-04-01 16:37:42 +0200

    make it better

--- helpers.py
+++ helpers.py
@@ -26,7 +26,8 @@ def loc(lon, lat):
     point = Point(lon, lat)
 
     # reading csv and making list of dict of circonscriptions
-    with open('/Users/arthur/Documents/MyCode/TrouverVotreDéputé.fr/static/circonscriptions.csv', mode='r') as inp:
+    with open('/Users/arthur/Documents/MyCode/TrouverVotreDéputé/static/circonscriptions.csv', mode='r') as inp:
+        
         reader = csv.reader(inp)
         # next(reader, None)
         circonscriptions = [{'name':row[0],'loc':(row[1])} for row in reader]
